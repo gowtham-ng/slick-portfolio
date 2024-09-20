6330947cbd27d4d722c5433862234f6314a5f0ec
Gowtham Gopinathan <142852484+gowtham-ng@users.noreply.github.com>
2024-09-20 15:37:50 +0000

    change education

--- src/lib/data/education.ts
+++ src/lib/data/education.ts
@@ -3,25 +3,25 @@ import type { Education } from '../types';
 
 export const items: Array<Education> = [
 	{
-		degree: 'Bachelor degree of Computer Science',
+		degree: "Bachelor's degree in Computer Science",
 		description: '',
-		location: 'Tunisia',
+		location: 'India',
 		logo: Assets.Unknown,
 		name: '',
-		organization: 'ISTIC',
-		period: { from: new Date(2020, 0, 1), to: new Date(2022, 5, 1) },
+		organization: 'SASTRA University',
+		period: { from: new Date(2016, 11, 7), to: new Date(2020, 20, 5) },
 		shortDescription: '',
 		slug: 'dummy-education-item',
 		subjects: ['C', 'Algorithm', 'Algebra', 'Python', 'C++', 'Java', 'English']
 	},
 	{
-		degree: 'PhD of Computer Science',
+		degree: 'Masters Degree in Computer Science',
 		description: '',
 		location: 'USA',
 		logo: Assets.Unknown,
 		name: '',
-		organization: 'MIT',
-		period: { from: new Date(2023, 0, 1) },
+		organization: 'Arizona State University',
+		period: { from: new Date(2022, 17, 8), to: new Date(2024, 4, 5) },
 		shortDescription: '',
 		slug: 'dummy-education-item-2',
 		subjects: ['Assembly', 'Rust', 'Computer Architecture', 'Algorithms and Data structures']
